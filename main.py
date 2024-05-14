ad03cee4481f642eb44ca50ab58e4fabde664a65
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-14 19:48:37 +0200

    turn off functionality

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import menu
 from resources import resources
 
-# Ask the user what they would like
-user_choice = input("What would you like? (espresso/latte/cappuccino): ")
-print(user_choice)
+machine_on = True
+while machine_on:
+    # Ask the user what they would like
+    user_choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    print(user_choice)
 
-# User can print report of the resources
-if user_choice == "report":
-    print(f"Water: {resources['water']}ml\n"
-          f"Milk: {resources['milk']}ml\n"
-          f"Milk: {resources['coffee']}ml\n"
-          f"Money: ${resources['money']}")
+    # Turn coffee machine off
+    if user_choice == "off":
+        machine_on = False
+        print("Machined switched off")
 
+    # User can print report of the resources
+    if user_choice == "report":
+        print(f"Water: {resources['water']}ml\n"
+              f"Milk: {resources['milk']}ml\n"
+              f"Milk: {resources['coffee']}ml\n"
+              f"Money: ${resources['money']}")
+
+    # Check if there are enough resources
+    # Make a function that takes user_choice, menu and resources, as an input
 
 
 
