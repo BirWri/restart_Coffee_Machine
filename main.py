45cf37881fdcd3a739d307b57c1c128926e2badc
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-14 19:18:12 +0200

    print report of resources

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import menu
-import resources
+from resources import resources
 
 # Ask the user what they would like
 user_choice = input("What would you like? (espresso/latte/cappuccino): ")
 print(user_choice)
 
+# User can print report of the resources
 if user_choice == "report":
-    print(resources.resources)
+    print(f"Water: {resources['water']}ml\n"
+          f"Milk: {resources['milk']}ml\n"
+          f"Milk: {resources['coffee']}ml\n"
+          f"Money: $")
 
 
 
