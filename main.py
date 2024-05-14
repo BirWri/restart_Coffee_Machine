542a66953edb33aed2e7117080c7740110757b42
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-14 18:54:56 +0200

    report

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import menu
+import resources
 
 # Ask the user what they would like
 user_choice = input("What would you like? (espresso/latte/cappuccino): ")
 print(user_choice)
+
+if user_choice == "report":
+    print(resources.resources)
+
 
 
 
