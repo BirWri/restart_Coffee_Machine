42968076ed0736f6443223902dc9c3536f2c17c6
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-15 12:50:23 +0200

    final off functionality and start of check resources function

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import menu
+from menu import MENU
 from resources import resources
+
+def check_resources(drink, menu, resources):
+    print(menu[drink]['ingredients']['water'])
+
 
 machine_on = True
 while machine_on:
@@ -11,16 +15,18 @@ while machine_on:
     if user_choice == "off":
         machine_on = False
         print("Machined switched off")
+    else:
 
-    # User can print report of the resources
-    if user_choice == "report":
-        print(f"Water: {resources['water']}ml\n"
-              f"Milk: {resources['milk']}ml\n"
-              f"Milk: {resources['coffee']}ml\n"
-              f"Money: ${resources['money']}")
+        # User can print report of the resources
+        if user_choice == "report":
+            print(f"Water: {resources['water']}ml\n"
+                  f"Milk: {resources['milk']}ml\n"
+                  f"Milk: {resources['coffee']}ml\n"
+                  f"Money: ${resources['money']}")
 
-    # Check if there are enough resources
-    # Make a function that takes user_choice, menu and resources, as an input
+        # Check if there are enough resources
+        # Make a function that takes user_choice, menu and resources, as an input
+        check_resources(drink=user_choice, menu = MENU, resources=resources)
 
 
 
