36674b06d38fa9f12680697910958fb8dbc4b6cd
BirWri <55280458+BirWri@users.noreply.github.com>
2024-05-15 16:13:20 +0200

    check resources

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from menu import MENU
 from resources import resources
 
-def check_resources(drink, menu, resources):
-    print(menu[drink]['ingredients']['water'])
 
+def check_resources(drink, menu, resources):
+
+    resource_available = True
+    if (menu[drink]['ingredients']['water']) > resources['water']:
+        print("Sorry there is not enough water.")
+        resource_available = False
+
+    if (menu[drink]['ingredients']['coffee']) > resources['coffee']:
+        print("Sorry there is not enough coffee.")
+        resource_available = False
+
+    if (menu[drink]['ingredients']['milk']) > resources['milk']:
+        print("Sorry there is not enough milk.")
+        resource_available = False
+
+    return resource_available
 
 machine_on = True
 while machine_on:
@@ -26,7 +40,13 @@ while machine_on:
 
         # Check if there are enough resources
         # Make a function that takes user_choice, menu and resources, as an input
-        check_resources(drink=user_choice, menu = MENU, resources=resources)
+        resources_quantity = check_resources(drink=user_choice, menu = MENU, resources=resources)
+
+        if resources_quantity == False:
+            machine_on = False
+        else:
+            # Ask coins
+
 
 
 
